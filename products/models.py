3d2cb559040ee70020331e00398240581b34289e
Michelle Clement <michellelclement@gmail.com>
2020-10-15 08:05:28 +0000

    customised admin

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
 
     class Meta:
+        # Corrects the name from Catagorys to plural
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
