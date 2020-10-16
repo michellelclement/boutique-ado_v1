ff58a221d5293fb2c0a7fcddf27eaeb70ebdbc64
Michelle Clement <michellelclement@gmail.com>
2020-10-16 15:37:03 +0000

    shopping bag in context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,26 @@
 from decimal import Decimal # Using decimal over float is preffered 
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    # To get bag from session
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # Free delivery if they spend over the amount speicifed in settings.py
     if total < settings.FREE_DELIVERY_THRESHOLD:
