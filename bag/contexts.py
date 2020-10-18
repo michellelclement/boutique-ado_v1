787d0b7aa60742c1de5fabe14819b3bb2f4f04e6
Michelle Clement <michellelclement@gmail.com>
2020-10-18 08:37:38 +0000

    finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,16 +11,31 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    # To get bag from session
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    # To get bag from session (code before the for if)
+    for item_id, item_data in bag.items():
+        # execute code if item has no sizes by seeing if int.
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            # Else it is a dic and need to be handled differently
+            # Iterate through inner dics of item_by_size & also add size to bag items template too
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     # Free delivery if they spend over the amount speicifed in settings.py
     if total < settings.FREE_DELIVERY_THRESHOLD:
