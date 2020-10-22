cb4eded47c4c4344330f897d722a681e59b63f7b
Michelle Clement <michellelclement@gmail.com>
2020-10-22 09:47:37 +0000

    testing webhooks, has error. Restarting

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -33,10 +33,6 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success!')
-    return HttpResponse(status=200)
-
-    """
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
@@ -56,5 +52,3 @@ def webhook(request):
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
-    """ 
