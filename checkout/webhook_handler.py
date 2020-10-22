a6641ae889a5f2d944a5e4e0c43644cd51afe14b
Michelle Clement <michellelclement@gmail.com>
2020-10-22 14:25:59 +0000

    Added checkout form data cashing in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,15 +15,15 @@ class StripeWH_Handler:
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
-
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle payment intent succeeded webhook from stripe
+        Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-
 
     def handle_payment_intent_payment_failed(self, event):
         """
