8e7179f5873525068e39ac63ad2fc3a607e15073
Michelle Clement <michellelclement@gmail.com>
2020-10-23 15:11:20 +0000

    added profile form, views and updated template

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    # Post handler for profile view
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    # Render order history on this page
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        # Delete profile when we render the profile
+        # 'profile': profile,
+        # Add below for rendering order history
+        'orders': orders,
+        # To stop update profile message including bag contents
+        'on_profile_page': True
     }
 
     return render(request, template, context)
