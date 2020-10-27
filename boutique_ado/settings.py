e5b67e550afdff020bef8e6c144af427e8a334d2
Michelle Clement <michellelclement@gmail.com>
2020-10-27 17:56:06 +0000

    Small amend to assist deployment of static files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -122,7 +122,7 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
-# Database
+# Database.
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
